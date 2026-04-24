c1a95d02a316664fd5985cac23d708d0d607d672
amanupadhy480-josh <amanupadhy480@gmail.com>
2026-04-25 02:37:21 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,76 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO, send
+import os
+from flask import Flask, render_template, redirect, url_for, request, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_socketio import SocketIO, emit
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'mysecret'
-socketio = SocketIO(app, cors_allowed_origins="*")
+app.config['SECRET_KEY'] = 'dev-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///chat.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+db = SQLAlchemy(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+
+# Database Models
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(500), nullable=False)
+    sender = db.Column(db.String(50), nullable=False)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+# Routes
 @app.route('/')
+@login_required
 def index():
-    return render_template('index.html')
+    messages = Message.query.all()
+    return render_template('index.html', saved_messages=messages)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('index'))
+        flash('Invalid Username or Password')
+    return render_template('login.html')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        hashed_pw = generate_password_hash(password, method='pbkdf2:sha256')
+        new_user = User(username=username, password=hashed_pw)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('signup.html')
 
 @socketio.on('message')
 def handleMessage(msg):
-    print('Message: ' + msg)
-    send(msg, broadcast=True)
+    new_msg = Message(content=msg, sender=current_user.username)
+    db.session.add(new_msg)
+    db.session.commit()
+    # Broadcast sender name also
+    emit('message', {'msg': msg, 'user': current_user.username}, broadcast=True)
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     socketio.run(app)
-
